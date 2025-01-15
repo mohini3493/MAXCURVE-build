6d4e79572eaf8ba4dcd495513064349c8c76fd2b
Mohini Digiligo <mohini3493@gmail.com>
2025-01-15 18:44:51 +0530

    fixed 19+

---
+++ _next/static/chunks/app/ai/page-5d0adf7578a5f6a0.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[867,160,327,469,457,808,883],{2872:function(e,n,s){Promise.resolve().then(s.bind(s,2149)),Promise.resolve().then(s.t.bind(s,8326,23))}},function(e){e.O(0,[278,149,971,472,744],function(){return e(e.s=2872)}),_N_E=e.O()}]);
