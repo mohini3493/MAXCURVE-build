7220f334938ee9f4fc378a7076881133d5445d62
Mohini Digiligo <mohini3493@gmail.com>
2025-02-04 16:19:32 +0530

    added job apply form

---
+++ _next/static/chunks/app/services/page-d0efbdcbe45b7de4.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[469,160,883,457,808,867],{2872:function(e,n,s){Promise.resolve().then(s.bind(s,2149)),Promise.resolve().then(s.t.bind(s,8326,23))}},function(e){e.O(0,[278,149,971,472,744],function(){return e(e.s=2872)}),_N_E=e.O()}]);
